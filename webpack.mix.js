15542ba55c3a398b468c313fb69d23499bd9cf8d
Daniel Schmitz <daniel@bashgeek.net>
2019-11-06 15:08:08 +0800

    moo

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
 const mix = require('laravel-mix')
 
-mix.sass('resources/sass/field.scss', 'dist/field.css');
-mix.js('resources/js/field.js', 'dist/field.js');
+mix.sass('resources/sass/field.scss', 'dist/css/field.css');
+mix.js('resources/js/field.js', 'dist/js/field.js');
