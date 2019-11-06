396a764636682c6a24f1bed353bc8492f37498b3
Daniel Schmitz <daniel@bashgeek.net>
2019-11-06 16:21:45 +0800

    Back to old mix version

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
-const mix = require('laravel-mix')
+// const mix = require('laravel-mix')
+//
+// mix.sass('resources/sass/field.scss', 'dist/css/field.css');
+// mix.js('resources/js/field.js', 'dist/js/field.js');
 
-mix.sass('resources/sass/field.scss', 'dist/css/field.css');
-mix.js('resources/js/field.js', 'dist/js/field.js');
+let mix = require('laravel-mix')
+
+mix.setPublicPath('dist')
+	.js('resources/js/field.js', 'js')
+	.sass('resources/sass/field.scss', 'css')
